945f51c531801600f4f5c4f967680a40b1fcfe96
Josueio <josuelomorejon333@gmail.com>
2026-04-01 21:46:37 -0500

    Fix: Disable SQL echo to avoid log overflow

--- fastapi_project/database.py
+++ fastapi_project/database.py
@@ -14,7 +14,7 @@ connect_args = {}
 if database_url.startswith("sqlite"):
     connect_args = {"check_same_thread": False}
 
-engine = create_engine(database_url, echo=True, connect_args=connect_args)
+engine = create_engine(database_url, echo=False, connect_args=connect_args)
 
 def create_db_and_tables():
     try:
